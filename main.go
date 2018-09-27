cd0f139bbd10ea5445e0d8e90d2ccc68a4bd2a44
FenwickElliott <charles@fenwickelliott.io>
2018-09-27 15:56:58 -0400

    age

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 
-	project "github.com/fenwickelliott/project-models"
+	project "github.com/fenwickelliott/project/project-models"
 )
 
 func main() {
 	thing := project.Thing{
 		Declaration: "I be a API",
+		Number:      13,
 	}
 
 	fmt.Println(thing.Declaration)
+	fmt.Printf("I am %d minutes old\n", thing.Number)
 }
